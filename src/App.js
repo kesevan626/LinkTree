77064fdb4d86a9647f4d107a4d4781c3c01d9b6e
kesevan626 <87601398+kesevan626@users.noreply.github.com>
2022-10-20 19:54:34 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import './App.css';
+import image from './IMG_2333.JPG'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
+    <div className="container">
+      <div className="main-section">
+          <div className="content">
+            <div className="profile-section">
+                <img src={image} id="profile_img" alt="" />
+                <div id="twitter">kesevenHayden</div>
+                <h3 hidden id="slack">Jacob Peter</h3>
+            </div>
+            <div className="link-section">
+                <a href="https://training.zuri.team/"><button className="btn">Twitter Link</button></a>
+                <a href="https://training.zuri.team/"><button className="btn" id="btn__zuri">Zuri Teams</button></a>
+                <a href="http://books.zuri.team"><button className="btn" id='books'>Zuri Books</button></a>
+                <a href="https://books.zuri.team/python-for-beginners?ref_id=<yourslackname>">
+                  <button className="btn" id="book__python">
+                    Python Books
+                  </button>
+                </a>
+                <a href="https://background.zuri.team"><button className="btn" id="pitch">Background checks for color</button></a>
+                <a href="https://books.zuri.team/design-rules"><button className="btn" id="book__design">Design Books</button></a>
+            </div>
+            <div className="social-section">
+              <img src="" alt="" />
+              <img src="" alt="" />
+            </div>
+          </div>
+      </div>
+      <div className="footer">
+        <div className="container">
+          <div className="content">
+              <p>Zuri<span>.</span> Internship </p>
+              <p className="text-muted">HNG Internship 9 Frontend Task</p>
+              <p>Ingressive for Good</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
